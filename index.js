e8cfce8db1f8058e5f89dff0c5b8408dcfc8328e
Jeandre Coetzee <Jeandre@Alvirnos-MacBook-Air.local>
2025-08-10 18:37:48 +0200

    Fix: Add index.js entry point to properly register the app component

---
+++ index.js
@@ -0,0 +1,6 @@
+import { registerRootComponent } from 'expo';
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that you render the app container fully off-screen before rendering it on screen.
+registerRootComponent(App);
